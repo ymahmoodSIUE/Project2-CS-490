8e8c31e94c673f7292051569cf1f85c44f7cbb7a
ymahmoodSIUE <123699824+ymahmoodSIUE@users.noreply.github.com>
2024-04-26 00:01:08 -0500

    Update one_hot_encoder.py

--- one_hot_encoder.py
+++ one_hot_encoder.py
@@ -7,18 +7,18 @@ def onehote(seq):
         encoding.append(mapping.get(base, [0, 0, 0, 0]))
     return np.array(encoding)
 
-# Open the input file
+
 with open("validation_file.txt", "r") as input_file:
-    encoded_sequences = []  # List to store the encoded sequences
-    # Loop through each line (sequence) in the input file
+    encoded_sequences = []  
+    
     for line in input_file:
-        sequence = line.strip()  # Remove leading/trailing whitespace, including newline
+        sequence = line.strip()  
         oneHotEncodedSequence = onehote(sequence)
         encoded_sequences.append(oneHotEncodedSequence)
 
-# Convert the list of encoded sequences to a numpy array
+
 encoded_sequences_array = np.array(encoded_sequences)
 
-# Save the numpy array to a .npy file
+
 np.save("validationout.npy", encoded_sequences_array)
 
